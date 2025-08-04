0773b833a1523f8390b8459747594af862ef7ed3
leoptv38 <leoptv38@gmail.com>
2025-08-04 20:31:08 +0500

    Delete admin/ajax directory

--- admin/ajax/cron_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ClickerVolt;
-
-require_once __DIR__ . '/../../../../wp-load.php';
-require_once __DIR__ . '/../../../../wp-includes/load.php';
-require_once __DIR__ . '/cron.php';
-
-Cron::processClicksQueue(true);
--- admin/ajax/tools/precacheDevices.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ClickerVolt;
-
-require_once __DIR__ . '/../../utils/deviceDetection.php';
-
-DeviceDetection::reCache();
